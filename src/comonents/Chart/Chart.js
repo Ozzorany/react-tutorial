cd81bbb1b61fa224cecd5e088c25204c324d3b85
Oz Zorany <ozzorany@gmail.com>
2021-12-30 19:37:13 +0200

    creating charts

---
+++ src/comonents/Chart/Chart.js
@@ -0,0 +1,20 @@
+import './Chart.css';
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+    const dataPointValues = props.dataPoints.map(point => point.value);
+    const totalMaximum = Math.max(...dataPointValues);
+
+    return (
+        <div className={"chart"}>
+            {props.dataPoints.map(point =>
+                <ChartBar key={point.label}
+                          value={point.value}
+                          maxValue={totalMaximum}
+                          label={point.label}/>
+            )}
+        </div>
+    );
+}
+
+export default Chart;
--- src/comonents/Expenses/Expenses.js
+++ src/comonents/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import {useState} from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -21,8 +22,8 @@ const Expenses = (props) => {
             <Card className={"expenses"}>
                 <ExpensesFilter
                     selected={filteredYear}
-                    onChangeFilter={filterChangeHandler}
-                />
+                    onChangeFilter={filterChangeHandler}/>
+                <ExpensesChart expenses={filteredExpenses}/>
                 <ExpensesList items={filteredExpenses}>
                 </ExpensesList>
             </Card>
