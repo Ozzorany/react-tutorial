400f71b6b950a9919a9fe142225e446c2edcb52f
Oz Zorany <ozzorany@gmail.com>
2021-12-30 16:06:26 +0200

    conditional content

--- src/comonents/Expenses/Expenses.js
+++ src/comonents/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import './Expenses.css';
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import {useState} from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -15,18 +16,6 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent = <p>No Expenses found.</p>;
-
-    if (!!filteredExpenses.length) {
-        expensesContent = filteredExpenses.map(expense => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ))
-    }
 
     return (
         <div>
@@ -35,7 +24,8 @@ const Expenses = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {expensesContent}
+                <ExpensesList items={filteredExpenses}>
+                </ExpensesList>
             </Card>
         </div>
     );
