10a2d395b655b0c6f02bbb03b92be8b5003c1cfa
Oz Zorany <ozzorany@gmail.com>
2021-12-30 14:09:55 +0200

    rendering list

--- src/comonents/Expenses/Expenses.js
+++ src/comonents/Expenses/Expenses.js
@@ -16,27 +16,16 @@ const Expenses = (props) => {
             <Card className={"expenses"}>
                 <ExpensesFilter
                     selected={filteredYear}
-                    onChangeFilter={filterChangeHandler}/>
-                <ExpenseItem
-                    title={props.items[0].title}
-                    amount={props.items[0].amount}
-                    date={props.items[0].date}>
-                </ExpenseItem>
-                <ExpenseItem
-                    title={props.items[1].title}
-                    amount={props.items[1].amount}
-                    date={props.items[1].date}>
-                </ExpenseItem>
-                <ExpenseItem
-                    title={props.items[2].title}
-                    amount={props.items[2].amount}
-                    date={props.items[2].date}>
-                </ExpenseItem>
-                <ExpenseItem
-                    title={props.items[3].title}
-                    amount={props.items[3].amount}
-                    date={props.items[3].date}>
-                </ExpenseItem>
+                    onChangeFilter={filterChangeHandler}
+                />
+                {props.items.map(expense => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))}
             </Card>
         </div>
     );
